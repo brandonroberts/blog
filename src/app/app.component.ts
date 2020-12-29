456d9c582545112d17589678414eefc41cbf4591
Brandon <robertsbt@gmail.com>
2020-12-29 09:13:54 -0600

    fix: change blog links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,7 @@ import { map } from 'rxjs/operators';
       >
         <mat-toolbar>Menu</mat-toolbar>
         <mat-nav-list>
-          <a mat-list-item linkTo="/" (click)="drawer.close()">Home</a>
+          <a mat-list-item linkTo="/blog" (click)="drawer.close()">Home</a>
           <a mat-list-item linkTo="/blog" (click)="drawer.close()">Posts</a>
           <a mat-list-item linkTo="/live" (click)="drawer.close()">Live Stream</a>
           <a mat-list-item linkTo="/about" (click)="drawer.close()"
@@ -40,7 +40,7 @@ import { map } from 'rxjs/operators';
             <mat-icon aria-label="Side nav toggle icon">menu</mat-icon>
           </button>
 
-          <a linkTo="/">Brandon Roberts</a>
+          <a linkTo="/blog">Brandon Roberts</a>
 
           <div class="social">
             <a *ngIf="!(isHandset$ | async)" linkTo="/blog">Posts</a>
