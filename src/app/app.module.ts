58e916d0091504abce7a6a666f0976c2a3d07865
Brandon <robertsbt@gmail.com>
2021-05-28 08:15:31 -0500

    chore: update to Angular 11.x

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,7 +26,7 @@ export class ContentComponent {}
         { path: 'blog/posts/:id', component: ContentComponent },
         { path: ':pageId', component: ContentComponent },
       ],
-      { initialNavigation: false }
+      { initialNavigation: 'disabled', relativeLinkResolution: 'legacy' }
     ),
     FooterComponentModule,
     HeaderComponentModule,
