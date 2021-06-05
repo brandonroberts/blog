b0a95c56d271304715b797841a71a61fb5809eaa
Brandon <robertsbt@gmail.com>
2021-06-04 22:14:15 -0500

    refactor: switch to native tailwind support

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,21 @@
-module.exports = (isProd) => ({
-  prefix: '',
-  purge: {
-    enabled: isProd,
-    content: ['**/*.{html,ts}'],
-  },
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-});
+const { guessProductionMode } = require("@ngneat/tailwind");
+
+process.env.TAILWIND_MODE = guessProductionMode() ? 'build' : 'watch';
+
+module.exports = {
+    prefix: '',
+    mode: 'jit',
+    purge: {
+      content: [
+        './src/**/*.{html,ts,css,scss,sass,less,styl}',
+      ]
+    },
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+};
