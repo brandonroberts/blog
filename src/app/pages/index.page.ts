d3768757725108cb93be46276125677f7953c3f8
Brandon Roberts <robertsbt@gmail.com>
2023-05-19 14:19:48 -0500

    refactor: update home redirect

--- src/app/pages/index.page.ts
+++ src/app/pages/index.page.ts
@@ -1,20 +1,6 @@
 import { RouteMeta } from '@analogjs/router';
-import { Component, inject } from '@angular/core';
-import { Router } from '@angular/router';
 
 export const routeMeta: RouteMeta = {
-  canActivate: [
-    () => {
-      const router = inject(Router);
-      router.navigate(['/blog']);
-      return false;
-    }
-  ]
+  redirectTo :'/blog',
+  pathMatch: 'full'
 };
-
-@Component({
-  selector: 'app-home',
-  standalone: true,
-  template: ``
-})
-export default class HomeComponent {}
