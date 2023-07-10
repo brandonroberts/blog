f0790a8f9c87ace0311896aae76fb736a217d0e6
Brandon Roberts <robertsbt@gmail.com>
2023-07-10 18:43:00 -0500

    feat: add full RSS example

--- src/server/routes/rss.xml.ts
+++ src/server/routes/rss.xml.ts
@@ -1,12 +1,44 @@
 //server/routes/rss.xml.ts
 
 import { defineEventHandler } from 'h3';
+import RSS from 'rss';
+import * as fs from 'fs';
+import * as path from 'path';
+import fm from 'front-matter';
+const posts = fs.readdirSync('./src/content');
+async function generateRssFeed() {
+ const site_url = 'https://brandonroberts.dev';
 
-export default defineEventHandler((event) => {
-  const feedString = `<?xml version="1.0" encoding="UTF-8"?>
-<rss version="2.0">
-</rss>
-  `;
+ const feedOptions = {
+  title: 'Brandon Roberts Blog | RSS Feed',
+  description: 'Notes to my future self.',
+  site_url: site_url,
+  feed_url: `${site_url}/api/rss.xml`,
+  image_url: `${site_url}/assets/images/brandonroberts.jpg`,
+  pubDate: new Date(),
+  copyright: `All rights reserved ${new Date().getFullYear()}`,
+ };
+
+ const feed = new RSS(feedOptions);
+
+ posts.forEach((postFile) => {
+  
+  const fileContents = fs.readFileSync(path.resolve('src/content', postFile), 'utf8');
+  const post: any = fm(fileContents).attributes;  
+  
+    feed.item({
+      title: post.title,
+      description: post.description,
+      url: `${site_url}/blog/posts/${post.slug}`,
+      date: post.publishedDate,
+    });
+  });
+
+  return feed.xml({ indent: true });
+}
+
+export default defineEventHandler(async(event) => {
+  const feedString = await generateRssFeed();
   event.node.res.setHeader('content-type', 'text/xml');
   event.node.res.end(feedString);
 });
