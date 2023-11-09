87b04098c79a69cce0189277d0e97cbefa372849
Brandon Roberts <robertsbt@gmail.com>
2023-11-08 22:48:53 -0600

    chore: add changelog generation

--- release.config.js
+++ release.config.js
@@ -7,13 +7,24 @@ module.exports = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     [
+      "@semantic-release/changelog",
+      {
+        "changelogFile": "CHANGELOG.md"
+      }
+    ],    
+    [
       '@semantic-release/github',
       {
         'assets': ['dist/analog/public'],
         successComment: false,
       }
     ],
-    '@semantic-release/git'
+    [
+      "@semantic-release/git",
+      {
+        "assets": ["docs/CHANGELOG.md"]
+      }
+    ]
   ],
   'preset': 'angular'
 };
