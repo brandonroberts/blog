ce04721c8d7293a7cefdc27b504ff8041d414242
Brandon Roberts <robertsbt@gmail.com>
2023-11-21 21:36:53 -0600

    feat: add separate package.json update to releases

--- release.config.js
+++ release.config.js
@@ -18,8 +18,21 @@ module.exports = {
       {
         'npmPublish': false
       }
-    ],  
-    '@semantic-release/git'
+    ],
+    [
+      '@semantic-release/npm',
+      {
+        'npmPublish': false,
+        'pkgRoot': './projects/my-lib/package.json'
+      }
+    ],     
+    [
+      '@semantic-release/git',
+      { 
+        'assets': ['CHANGELOG.md', 'package.json', 'projects/my-lib/package.json'],
+        'message': 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}' 
+      }
+    ]
   ],
   'preset': 'angular'
 };
