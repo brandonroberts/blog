3b1512205722102c4120781f68a3756f17a65f53
Brandon Roberts <robertsbt@gmail.com>
2024-04-18 14:14:44 -0500

    feat: update to Analog 1.2.2

--- src/app/pipes/reading-time.pipe.ts
+++ src/app/pipes/reading-time.pipe.ts
@@ -7,7 +7,7 @@ import readingTime from 'reading-time';
   standalone: true,
 })
 export class ReadingTimePipe implements PipeTransform {
-  transform(content: string | undefined): number {
-    return content ? Math.round(readingTime(content).minutes) : 0;
+  transform(content: string | undefined | object): number {
+    return content ? Math.round(readingTime(content as string).minutes) : 0;
   }
 }
