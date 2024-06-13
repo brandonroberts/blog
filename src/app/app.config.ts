a427968aeb792366d30af02ea1206a43c95d046c
Brandon Roberts <robertsbt@gmail.com>
2024-06-13 09:12:58 -0500

    feat: upgrade to Angular v18 and Analog 1.5.0

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,13 @@ import { ApplicationConfig } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideFileRouter } from '@analogjs/router';
 import { provideContent, withMarkdownRenderer } from '@analogjs/content';
+import { withPrismHighlighter } from "@analogjs/content/prism-highlighter";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideFileRouter(),
     provideHttpClient(),
     provideClientHydration(),
-    provideContent(withMarkdownRenderer())
+    provideContent(withMarkdownRenderer(), withPrismHighlighter())
   ],
 };
